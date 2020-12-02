b0af42f47df15e630112a891201505e5a3372f05
fazzyA <faz.pak@gmail.com>
2020-12-02 22:52:42 +0500

    class 1 JS

---
+++ index.js
@@ -0,0 +1,84 @@
+// console.log('I am in JS class')
+// // alert('I am in JS class')
+// /// 3 ways to declare variables
+// let a = 10;
+// var b = 20;
+// const c =30;
+// // let user_input = prompt('enter ur name', 'faiza')
+// // console.log(user_input)
+// let fruit = 'apple'
+// {
+//     let fruit  = 'mango'
+//     {
+//         let fruit = 'cherry'
+//         console.log(fruit)
+//     }
+//     console.log(fruit) //mango apple
+// }
+// var age = '18';
+// var name = 'hira'
+// // if(name === 'faiza'){
+// //     console.log('you r allowed')
+// //     } else{
+// //         console.log('you r not allowed')
+    
+// //     }
+// /// nested if else && ||
+// if(age == 18 || name == 'faiza'){
+//     console.log('you r allowed')
+// } else{
+//     console.log('you r not allowed')
+  
+// }
+//     // if(age == 18){
+//     //     if(name=='faiza'){
+//     //                console.log('inside child if')
+ 
+//     //     } else{
+//     //                 console.log('inside main if')
+
+//     //     }
+//     //     } else{
+//     //         console.log('you r not allowed')
+        
+//     //     }
+// /// reference error / undefined
+// let xyz;
+// console.log('i am in class'+ ' hello world')
+// /// string
+// var str = 'quick brown fox';
+// str = str.toUpperCase()
+// str = str.toLowerCase()
+// console.log(str)
+// var part = str.slice(0, 1)
+// part = part.toUpperCase()
+// let newStr = part + str.slice(1);
+// console.log(newStr)
+// /// template literals
+// var name = 'faiza';
+// var greet = 'good morning'
+// var hello = 'hello'
+// console.log(`${name} ${greet} ${hello} hi i am in class`)
+// console.log(`faiza
+// aziz
+// khan`)
+// ///ternary operator
+// var myAge = 18;
+// var CNIC; // yes , no
+
+// // condition ? '' : ''
+var input = prompt('numb')
+let response = input > 0 ? 'number is +ve' : 'number is -ve'
+console.log(response)
+//  myAge > 17 ? (console.log('true')) : (console.log('false'))
+// CNIC = myAge >=18 ? 'YES' : 'NO'
+// console.log(CNIC)
+// var userAge = prompt('enter ur age')
+// var days = userAge * 365
+// let num = 2;
+// console.log(++num) // 3
+// console.log(++num) //4
+// console.log(--num) //3
+// console.log(--num) //2
+// console.log(num+10) //2
+
